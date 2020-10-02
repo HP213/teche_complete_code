0c0f3464551ddda91992080be9a6f7cdd1080cac
Gurpreet singh <amansingh9569@gmail.com>
2020-10-02 10:25:36 +0530

    first

--- smartServer/settings.py
+++ smartServer/settings.py
@@ -10,6 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
+"""
+first
+"""
+
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
