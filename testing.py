3c2bac515c8a88bc87732dc8e22b0d06932f19d3
unknown <harshbansal1000@gmail.com>
2020-10-04 17:28:15 +0530

    TestChanges

--- testing.py
+++ testing.py
@@ -1,11 +1,12 @@
-import psutil
 import requests
+import psutil
 id = 45
 
 count = psutil.cpu_count()
 temp = psutil.sensors_temperatures()['coretemp'][0][1]
 
 url = 'http://localhost:8000/storedata'
+
 
 import time
 t=time.time()
